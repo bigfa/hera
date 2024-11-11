8b744bf075774e8f9aefedc62bbed622e8af2128
bigfa <jigoulee@gmail.com>
2024-11-11 17:20:43 +0800

    chore

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <main class="layout">
     <div class="navbar">
         <div class="js-contentFixed">
-            <img src="http://2.gravatar.com/avatar/2fd7e2e17a671f8e3fade0706e0a667e?s=100&d=mm&r=g" alt="bigfa">
+            <img src="http://2.gravatar.com/avatar/2fd7e2e17a671f8e3fade0706e0a667e?s=100&d=mm&r=g" alt="bigfa" class="avatar">
             <h1>bigfa</h1>
             <p>computer loser</p>
         </div>
-        <p>Made in <img alt="Japan" aria-label="Japan" src="<?php echo get_template_directory_uri(); ?>/build/images/Macao.svg" class="footer--logo"></span></p>
+        <div class="nav--footer">Made in <img alt="Japan" aria-label="Japan" src="<?php echo get_template_directory_uri(); ?>/build/images/Macao.svg" class="footer--logo"></span></div>
     </div>
     <div class="content">
         <div class="post--list">
