fac09587c29f214f33cd2864584be73ca7a3cd60
bigfa <jigoulee@gmail.com>
2024-11-22 15:01:58 +0800

    chore

--- footer.php
+++ footer.php
@@ -1,11 +1,5 @@
 </div>
 </main>
-<footer class="footer">
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-        <p>Made in <img alt="Japan" aria-label="Japan" src="<?php echo get_template_directory_uri(); ?>/build/images/Macao.svg" class="footer--logo"></span></p>
-    </div>
-</footer>
 </body>
 
 <?php wp_footer(); ?>
--- template-parts/content.php
+++ template-parts/content.php
@@ -7,7 +7,7 @@
                 echo $sippnet;
                 ?>
             </div>
-            <p><?php the_author(); ?> on <?php the_time('F j, Y'); ?> <?php the_category(' '); ?></p>
+            <p><?php the_time('Y-m-d'); ?> · <?php the_category(' '); ?></p>
         </div>
         <img src="<?php echo aladdin_get_background_image(get_the_ID(), 84, 84); ?>" class="cover" />
     </div>
