e6c6cacb81e1be13e9f7cffab7fe6eee97db4827
bigfa <jigoulee@gmail.com>
2024-12-02 17:36:27 +0800

    chore

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="site--footer u-textAligncenter">
-    <div>2024 just a bigfa theme. blog since 2013</div>
+    <div>2024 blog since 2013</div>
     <div class="nav--footer">
-        Made in <img alt="Japan" aria-label="Japan" src="<?php echo get_template_directory_uri(); ?>/build/images/Macao.svg" class="footer--logo"></span>
+        Just a bigfa theme. Originally made in <img alt="Japan" aria-label="Japan" src="<?php echo get_template_directory_uri(); ?>/build/images/Macao.svg" class="footer--logo"></span>
     </div>
 </footer>
 </div>
