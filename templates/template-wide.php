c21dcb871003c56e2517d049a787554f7f140fa5
bigfa <jigoulee@gmail.com>
2024-12-02 14:50:53 +0800

    chore

--- templates/template-wide.php
+++ templates/template-wide.php
@@ -4,24 +4,28 @@ Template Name: Wide
 */
 
 get_header(); ?>
-<?php if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-        <article class="post">
-            <header>
-                <h2 class="post--headline"><?php the_title(); ?></h2>
-                <div class="meta">
-                    <span>作者：<?php the_author(); ?></span>
-                    <span>时间：<?php the_time('Y-m-d'); ?></span>
-                    <span>分类：<?php the_category(','); ?></span>
-                    <span>标签：<?php the_tags('', ','); ?></span>
+<div class="articleContainer">
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+            <article class="post">
+                <header>
+                    <h2 class="post--headline"><?php the_title(); ?></h2>
+                    <div class="meta">
+                        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" class="author">
+                            <img src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>" alt="<?php the_author(); ?>" class="avatar">
+                            <span><?php the_author(); ?></span>
+                        </a>
+                        <span><?php the_time('Y-m-d'); ?></span>
+                        <span class="sep"></span>
+                        <span><?php the_category(','); ?></span>
+                    </div>
+                </header>
+                <div class="grap">
+                    <?php the_content(); ?>
                 </div>
-            </header>
-            <div class="grap">
-                <?php the_content(); ?>
-            </div>
-        </article>
-
-<?php endwhile;
-// get_template_part('template-parts/pagination');
-endif; ?>
+            </article>
+    <?php endwhile;
+    // get_template_part('template-parts/pagination');
+    endif; ?>
+</div>
 <?php get_footer(); ?>
