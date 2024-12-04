40083cd3d8065a0abdb7b71f176c7d2066df63dc
bigfa <jigoulee@gmail.com>
2024-12-04 15:19:33 +0800

    chore

--- template-parts/author-card.php
+++ template-parts/author-card.php
@@ -3,7 +3,11 @@ global $farallonSetting; ?>
 <div class="author--card">
     <?php echo get_avatar(get_the_author_meta('ID'), 64); ?>
     <div>
-        <div class="author--name"><?php the_author(); ?></div>
+        <div class="author--name">
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" class="author">
+                <?php the_author(); ?>
+            </a>
+        </div>
         <div class="author--description"><?php the_author_meta('description'); ?></div>
     </div>
 </div>
