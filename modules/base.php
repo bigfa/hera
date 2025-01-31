71277579b55b15227f31422d7c28dfcb6424f934
Miss Taco <misstaco@msn.com>
2025-01-31 16:16:00 -0500

    fix: make image zoom respect theme settings

--- modules/base.php
+++ modules/base.php
@@ -138,6 +138,12 @@ class heraBass
 
     function panther_image_zoom($content)
     {
+        global $heraSetting;
+        // Check if image zoom is enabled in settings
+        if (!$heraSetting->get_setting('image_zoom')) {
+            return $content;
+        }
+        
         $pattern = "/<a(.*?)href=('|\")([^>]*).(bmp|gif|jpeg|jpg|png)('|\")(.*?)>(.*?)<\/a>/i";
         $replacement = '<a$1href=$2$3.$4$5 data-action="imageZoomIn" $6>$7</a>';
         $content = preg_replace($pattern, $replacement, $content);
